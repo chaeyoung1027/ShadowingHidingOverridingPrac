eb024cf846ea8dd1a2321c0565ec143b42321ddd
Chaeyoung Lim <rnldyal1027@naver.com>
2023-06-01 09:57:16 +0900

    초기 커밋.

---
+++ ShadowingHidingOverridingPrac/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowingHidingOverridingPrac
+{
+    internal class Program
+    {
+        public static int number = 10; //같은 이름의 지역변수에 의해 가려짐(클래스 변수)
+        public int number2 = 2; //같은 이름의 지역변수에 의해 가려짐(인스턴스 변수)
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);  //지역변수 number출력
+            Console.WriteLine(Program.number);  //이렇게 접근은 가능
+            Console.WriteLine((new Program()).number2); //인스턴스 변수인 경우는 이렇게
+        }
+    }
+}
